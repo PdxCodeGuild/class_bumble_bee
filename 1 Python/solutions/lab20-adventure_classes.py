82bb989baea708bd33cd162778f213300ede2ae0
flux <flux2341@gmail.com>
2020-12-28 13:57:24 -0800

    added lab 20 class example

--- 1 Python/solutions/lab20-adventure_classes.py
+++ 1 Python/solutions/lab20-adventure_classes.py
@@ -1,6 +1,6 @@
 
 
-
+# parent class which contains a location, character, and health
 class Creature:
 
     def __init__(self, location_i, location_j, character, health):
@@ -9,41 +9,50 @@ class Creature:
         self.character = character
         self.health = health
 
+# player class (which inherits from creature)
+# which defaults to having a smiley face as the character and 10 for the health
 class Player(Creature):
 
     def __init__(self, location_i, location_j):
-        super().__init__(location_i, location_j, '☺', 10)
-    
+        super().__init__(location_i, location_j, '☺', 10) # invoking the parent class's initializer
+
+# enemy class (which inherits from creature)
+# which defaults to having a squiggley as the character and 4 for the health
 class Enemy(Creature):
 
     def __init__(self, location_i, location_j):
-        super().__init__(location_i, location_j, '§', 4)
+        super().__init__(location_i, location_j, '§', 4) # invoking the parent class's initializer
     
 
+# board class which represents the board
 class Board:
 
     def __init__(self, width, height):
+
         self.width = width
         self.height = height
-        self.board = []
+        self.grid = [] # list of lists of strings
         self.creatures = []
+
         for i in range(self.height):
             row = []
             for j in range(self.width):
                 row.append(' ')
-            self.board.append(row)
+            self.grid.append(row)
     
     def add_creature(self, creature):
         self.creatures.append(creature)
 
     def print_board(self):
-        print('    ', end='')
+        print('  j ', end='')
         for j in range(self.width):
             print(j, end=' ')
         print()
-        print('   ' + '-'*self.width*2)
+        print('i  ' + '-'*self.width*2)
+        # loop over the rows
         for i in range(self.height):
             print(str(i) + ' |', end=' ')
+            # loop over the columns
             for j in range(self.width):
                 # find out if there's a creature at the given location
                 # if there is, print out its character
@@ -53,7 +62,7 @@ class Board:
                         print(creature.character, end=' ')
                         break
                 else:
-                    print(self.board[i][j], end=' ')  # otherwise print the board square
+                    print(self.grid[i][j], end=' ')  # otherwise print the board square
             print('|')
         print('   ' + '-'*self.width*2)
 
@@ -71,9 +80,21 @@ board.add_creature(enemy1)
 enemy2 = Enemy(1, 3)
 board.add_creature(enemy2)
 
-enemies = [enemy1, enemy2]
+enemy3 = Enemy(0, 0)
+board.add_creature(enemy3)
 
+enemies = [enemy1, enemy2, enemy3]
+
+
+
+
+
+
+
+
+# REPL
 while True:
+    board.print_board()
 
     command = input('what is your command? ')  # get the command from the user
 
@@ -93,7 +114,7 @@ while True:
         if player.location_i == enemy.location_i and player.location_j == enemy.location_j:
             print('You encountered an enemy!')
     
-    board.print_board()
+    
 
 
 

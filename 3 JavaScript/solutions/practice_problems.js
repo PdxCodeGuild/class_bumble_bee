2ae028b7d3fa5948c0dd25b74e4f32642a296a1d
flux <flux2341@gmail.com>
2021-01-13 10:42:07 -0800

    more practice problems

--- 3 JavaScript/solutions/practice_problems.js
+++ 3 JavaScript/solutions/practice_problems.js
@@ -136,6 +136,72 @@ function eveneven(nums) {
 
 
 
+
+
+// Problem 4
+// Write a function to copy all the elements of a list with value less than 10 to a new list and return it.
+
+
+function extract_less_than_ten(nums) {
+    // let new_list = []
+    // for (num of nums) {
+    //     if (num < 10) {
+    //         new_list.push(num)
+    //     }
+    // }
+    // return new_list
+
+
+    let new_list = []
+    for (let i=0; i<nums.length; i++) {
+        if (nums[i] < 10) {
+            new_list.push(nums[i])
+        }
+    }
+    return new_list
+}
+// console.log(extract_less_than_ten([2, 8, 12, 18])) // [2, 8]
+
+
+// for (let i=0, k=0; i<10; ++i) {
+//     for (let j=0; j<10; ++j, k++) {
+//         console.log(i, j, k)
+//     }
+// }
+
+
+
+
+
+// Problem 5
+// Write a function to find all common elements between two lists.
+
+function common_elements(nums1, nums2) {
+    let output = []
+    for (let i=0; i<nums1.length; i++) {
+        for (let j=0; j<nums2.length; j++) {
+            // if nums1[i] == nums2[i] and nums1[i] not in output:
+            if (nums1[i] === nums2[j] && !output.includes(nums1[i])) {
+                output.push(nums1[i])
+            }
+        }
+    }
+    return output
+
+    // let output = []
+    // for (num1 of nums1) {
+    //     for (num2 of nums2) {
+    //         if (num1 == num2 && !output.includes(num1)) {
+    //             output.push(num1)
+    //         }
+    //     }
+    // }
+    // return output
+
+}
+// console.log(common_elements([1, 2, 2, 3], [2, 3, 4])) // [2, 3]
+
+
 // Problem 7
 // Given a list of numbers, and a target number, find a pair of numbers from the list that sum to a target number. Optional: return a list of all pairs of numbers that sum to a target value.
 
@@ -161,4 +227,71 @@ function find_pair(nums, target) {
     return null
 }
 
-console.log(find_pair([5, 6, 2, 3], 7)) // [5, 2]
+// console.log(find_pair([5, 6, 2, 3], 7)) // [5, 2]
+
+
+
+
+
+// let nums = [5, 6, 7, 8]
+// nums.forEach(function(x) {
+//     console.log(x)
+// })
+
+
+// Given a string of letters, find the index of each capital letter and return it
+
+function findCapitalIndices(text) {
+    // make text all caps using toUpperCase - CODEWARSCODEWARS
+    // loop through the indices of the text
+    // compare each character of the original text to the corresponding character of the upper case text
+    // if they're equal, add the index to an output array
+
+
+    // 0123456789...
+    // CodEWaRsCodEWaRs
+    // CODEWARSCODEWARS
+
+    // let upper = text.toUpperCase()
+    // let output = []
+    // for (let i=0; i<text.length; i++) {
+    //     if (upper[i] === text[i]) {
+    //         output.push(i)
+    //     }
+    // }
+    // return output
+
+
+    // loop over the indices of the string
+    // compare each letter to the capital version of the letter
+    // if they're equal add the index to the output
+
+    // let output = []
+    // for (let i=0; i<text.length; ++i) {
+    //     if (text[i] == text[i].toUpperCase()) {
+    //         output.push(i)
+    //     }
+    // }
+    // return output
+
+    
+
+    // make an upper case alphabet
+    // loop through the indices of the text
+    // check if the upper case alphabet includes the character
+    // if it does, add the index to the output array
+
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let output = []
+    for (let i=0; i<text.length; ++i) {
+        if (alphabet.includes(text[i])) {
+            output.push(i)
+        }
+    }
+    return output
+
+
+
+}
+//                              0123456789
+console.log(findCapitalIndices('CodEWaRsCodEWaRs')) // [0, 3, 4, 6, 8, ...]

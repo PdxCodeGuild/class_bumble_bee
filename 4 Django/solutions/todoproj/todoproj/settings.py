cc5146e050cea5c8d0374caa7d64604759ba756d
flux <flux2341@gmail.com>
2021-02-16 13:10:40 -0800

    added todo proj with user system

--- 4 Django/solutions/todoproj/todoproj/settings.py
+++ 4 Django/solutions/todoproj/todoproj/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todoapp'
+    'todoapp',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_URL = 'users/login/'
--- 4 Django/solutions/todoproj/todoproj/urls.py
+++ 4 Django/solutions/todoproj/todoproj/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('todoapp.urls'))
+    path('users/', include('users.urls')),
+    path('', include('todoapp.urls')),
 ]
